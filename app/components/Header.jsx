71c63784a82276fabe788f5c529f4b7d8de809e3
s31w4n <sewan.khalni@gmail.com>
2024-09-24 03:04:23 +0330

    test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   const { userId } = auth();
   return (
     <>
-      <div className="nav bg-blue-700 py-4 px-6 flex item-center justify-between mb-5">
+      <div className="nav bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
         <div className="flex items-center">
           <Link href="/">
             <div className="text-lg uppercase font-bold text-white">
@@ -29,14 +29,6 @@ function Header() {
                 Sign Up
               </Link>
             </>
-          )}
-          {userId && (
-            <Link
-              href="profile"
-              className="text-gray-300 hover:text-white mr-4 flex items-center"
-            >
-              Profile
-            </Link>
           )}
           <div className="ml-auto">
             <UserButton afterSignOutUrl="/" />
